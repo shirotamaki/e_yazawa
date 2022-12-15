a5fdda4a5d95262e629db4571d11cd934ab04bee
shirotamaki <shiro.tamaki63@gmail.com>
2022-12-15 17:25:36 +0900

    First commit

---
+++ lib/e_yazawa.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "e_yazawa/version"
+
+module EYazawa
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/e_yazawa/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module EYazawa
+  VERSION = "0.1.0"
+end
