4114cecf0dd574408f3f568e2370a89d807d8867
shirotamaki <shiro.tamaki63@gmail.com>
2022-12-15 17:29:12 +0900

    メイン機能を実装

--- lib/e_yazawa.rb
+++ lib/e_yazawa.rb
@@ -1,8 +1,18 @@
 # frozen_string_literal: true
 
 require_relative "e_yazawa/version"
+require "tty-prompt"
+require_relative "./database"
 
 module EYazawa
   class Error < StandardError; end
-  # Your code goes here...
+
+  def self.advise
+    prompt = TTY::Prompt.new
+    your_name = prompt.ask("ホワット ユア ネーム？")
+    choices = Message.advise(your_name)
+    question = "YAZAWAにアスクしたいことをセレクト！"
+    answer = prompt.select(question, choices)
+    puts answer
+  end
 end
